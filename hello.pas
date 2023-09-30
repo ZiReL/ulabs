9552735b0d002f32863526e739db103147a400cb
Александр Свистунов <sasha12svist@mail.ru>
2023-09-30 09:08:11 +0300

    Изменил на запрос времени и пожелания

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  Print('Hello World!');
+  var x := ReadInteger('Введите время:');
+  Assert((x >= 0) and (x < 24));
+  case x of
+    4..10: Print('Доброе утро, мир!');
+    11..16: Print('Добрый день, мир!');
+    17..22: Print('Добрый вечер, мир!');
+  else Print('Доброй ночи, мир!');
+  end;
 end.
